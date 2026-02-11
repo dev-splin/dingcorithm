198bcc4cd2825053784cc097e6efa948f2e226cd
윤창현 <ychyun@saramin.co.kr>
2026-02-11 12:52:52 +0900

    5주차 : 05_03 - 올바른 괄호 문자열 만들기(풀이 완료)

--- 5sk_week/05_03_get_correct_parentheses.py
+++ 5sk_week/05_03_get_correct_parentheses.py
@@ -2,7 +2,6 @@ from collections import deque
 
 balanced_parentheses_string = "()))((()"
 
-
 def get_correct_parentheses(balanced_parentheses_string):
     # 1
     if not balanced_parentheses_string:
@@ -41,16 +40,20 @@ def get_correct_parentheses(balanced_parentheses_string):
     if stack:
         isCorrect = False
 
+    # 3
+    if isCorrect:
+        return u + get_correct_parentheses(v)
 
+    #4
+    result = "(" + get_correct_parentheses(v) + ")"
+    new_u = u[1:-1]
+    new_u = "".join(")" if char == "(" else "(" for char in new_u)
 
-
-
-
-    return
+    return result + new_u
 
 
 print(get_correct_parentheses(balanced_parentheses_string))  # "()(())()"가 반환 되어야 합니다!
 
 print("정답 = (((()))) / 현재 풀이 값 = ", get_correct_parentheses(")()()()("))
-print("정답 = ()()( / 현재 풀이 값 = ", get_correct_parentheses("))()("))
+print("정답 = ()()() / 현재 풀이 값 = ", get_correct_parentheses("()()()"))
 print("정답 = ((((()())))) / 현재 풀이 값 = ", get_correct_parentheses(')()()()(())('))
