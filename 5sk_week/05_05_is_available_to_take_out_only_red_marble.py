b55f79488d39c0f56cfaf223951e94ab9c12de14
윤창현 <ychyun@saramin.co.kr>
2026-03-02 23:26:31 +0900

    5주차 : 05_04 - 구슬 탈출(완료)

--- 5sk_week/05_05_is_available_to_take_out_only_red_marble.py
+++ 5sk_week/05_05_is_available_to_take_out_only_red_marble.py
@@ -12,61 +12,62 @@ game_map = [
 dc = [-1, 0, 1, 0]
 dr = [0, 1, 0, -1]
 
-def move(game_map, dir, col_row):
-    col, row = col_row
-    new_col, new_row = col + dc[dir], row + dr[dir]
+def move(game_map, dir, row, col):
+    count = 0
+    new_row, new_col = row + dr[dir], col + dc[dir]
 
     # 한 쪽 방향으로 벽이나 구멍에 닿을 때 까지 이동
-    while game_map[new_col][new_row] == ".":
-        col, row = new_col, new_row
-        new_col, new_row = col + dr[dir], row + dc[dir]
+    while game_map[new_row][new_col] != "#" and game_map[row][col] != "O":
+        count += 1
+        row, col = new_row, new_col
+        new_row, new_col = new_row + dr[dir], new_col + dc[dir]
 
-    return [col, row]
-
-
-def solve(game_map, turn):
-    if turn > 10:
-        return False
-
-    red, blue, hole = [], [], []
-
-    # red, blue, hole 위치 저장
-    for col in range(1, len(game_map) - 1):
-        for row in range(len(game_map[col])):
-            if game_map[col][row] == "B":
-                blue = [col, row]
-            elif game_map[col][row] == "R":
-                red = [col, row]
-            elif game_map[col][row] == "O":
-                hole = [col, row]
-
-    # 4방향 이동
-    for dir in range(4):
-        print("=== turn, dir ===", turn, dir)
-        # blue가 구멍에 들어가는 쪽으론 기울 수 없음
-        new_blue_col, new_blue_row = move(game_map, dir, blue)
-        if [new_blue_col, new_blue_row] == hole:
-            continue
-
-        # red가 구멍에 들어가면 종료
-        new_red_col, new_red_row = move(game_map, dir, red)
-        if [new_red_col, new_red_row] == hole:
-            return True
-
-        # red/blue 이동 후 다음 턴
-        game_map[new_blue_col][new_blue_row] = game_map[blue[0]][blue[1]]
-        game_map[new_red_col][new_blue_row] = game_map[red[0]][red[1]]
-
-        print(game_map)
-
-        solve_result = solve(game_map, turn + 1)
-        if solve_result:
-            return True
-
-    return False
+    return [row, col, count]
 
 def is_available_to_take_out_only_red_marble(game_map):
-    return solve(game_map, 1)
+    n,m = len(game_map), len(game_map[0])
+    visited = [[[[False for _ in range(m)] for _ in range(n)] for _ in range(m)] for _ in range(n)]
+
+    red_row, red_col, blue_row, blue_col = -1, -1, -1, -1
+    for row in range(n):
+        for col in range(m):
+            if game_map[row][col] == "R":
+                red_row, red_col = row, col
+            elif game_map[row][col] == "B":
+                blue_row, blue_col = row, col
+
+    queue = deque()
+    queue.append((red_row, red_col, blue_row, blue_col, 1))
+    visited[red_row][red_col][blue_row][blue_col] = True
+
+    while queue:
+        red_row, red_col, blue_row, blue_col, turn = queue.popleft()
+        if turn > 10:
+            break
+
+        for dir in range(4):
+            # blue가 구멍에 들어가는 쪽으론 기울 수 없음
+            new_blue_row, new_blue_col, blue_count = move(game_map, dir, blue_row, blue_col)
+            if game_map[new_blue_row][new_blue_col] == "O":
+                continue
+
+            # red가 구멍에 들어가면 종료
+            new_red_row, new_red_col, red_count = move(game_map, dir, red_row, red_col)
+            if game_map[new_red_row][new_red_col] == "O":
+                return True
+
+            # red, blue가 같은 위치인 경우 조금 움직인 구슬이 자리를 차지
+            if new_red_col == new_blue_col and new_red_row == new_blue_row:
+                if red_count > blue_count:
+                    new_red_col, new_red_row = new_red_col - dc[dir], new_red_row - dr[dir]
+                else:
+                    new_blue_col, new_blue_row = new_blue_col - dc[dir], new_blue_row - dr[dir]
+
+            if not visited[new_red_row][new_red_col][new_blue_row][new_blue_col]:
+                visited[new_red_row][new_red_col][new_blue_row][new_blue_col] = True
+                queue.append((new_red_row, new_red_col, new_blue_row, new_blue_col, turn + 1))
+
+    return False
 
 
 print(is_available_to_take_out_only_red_marble(game_map))  # True 를 반환해야 합니다
