4409b6c35553f145d208a84f7e9c6da2a2413463
윤창현 <ychyun@saramin.co.kr>
2026-03-12 12:29:26 +0900

    5주차 : 05_10 - 수식 최대화(DFS에서 연산자 조합만 분리)

--- 5sk_week/05_10_calc_max.js
+++ 5sk_week/05_10_calc_max.js
@@ -6,18 +6,58 @@
 // 연산자 우선 순위를 바꿔가며 전체 탐색 해야함
 // 연산자 우선 순위 조합을 만들고 마지막에 계산
 
-const basicOperators = ["+", "-", "*"];
-let maxNum = 0;
+function getUsedOperators(expression) {
+    const operators = [];
+    if (expression.includes("*")) {
+        operators.push("*")
+    }
+    if (expression.includes("+")) {
+        operators.push("+");
+    }
+    if (expression.includes("-")) {
+        operators.push("-");
+    }
 
-function DFS(expression, operators) {
-    if (operators.length === 3) {
-        let allSplit = expression.match(/\d+|[+\-*]/g);
-        
+    return operators;
+}
+
+function getOperatorsCombination(usedOperators) {
+    const operatorsCombination = [];
+
+    if (usedOperators.length === 0 ) {
+        operatorsCombination.push([]);
+
+        return operatorsCombination;
+    }
+
+    for (const operator of usedOperators) {
+        const filterdUsedOperators = usedOperators.filter(o => o !== operator);
+        const remainOperators = getOperatorsCombination(filterdUsedOperators);
+
+        for (const operators of remainOperators) {
+            const newOperators = [operator, ... operators];
+            operatorsCombination.push(newOperators);
+        }
+    }
+
+    return operatorsCombination;
+}
+
+function solution(expression) {
+    let answer = 0;
+
+    const usedOperators = getUsedOperators(expression);
+    const operatorsCombination = getOperatorsCombination(usedOperators);
+
+    const basicSplitExpression = expression.match(/\d+|[+\-*]/g);
+    for (const operators of operatorsCombination) {
+        const splitExpression = [...basicSplitExpression]
         for (const operator of operators) {
-            let findIndex = allSplit.findIndex(char => char === operator);
+            let findIndex = splitExpression.indexOf(operator);
+
             while(findIndex > 0) {
-                const prevNum = Number(allSplit[findIndex - 1]);
-                const afterNum = Number(allSplit[findIndex + 1]);
+                const prevNum = Number(splitExpression[findIndex - 1]);
+                const afterNum = Number(splitExpression[findIndex + 1]);
                 let calcNum = 0;
 
                 if (operator === "+") {
@@ -28,29 +68,18 @@ function DFS(expression, operators) {
                     calcNum = prevNum * afterNum;
                 }
 
-                allSplit = [...allSplit.slice(0, findIndex - 1), calcNum, ...allSplit.slice(findIndex + 2)];
-                findIndex = allSplit.findIndex(char => char === operator);
+                splitExpression[findIndex - 1] = calcNum;
+                splitExpression.splice(findIndex, 1);
+                splitExpression.splice(findIndex, 1);
+
+                findIndex = splitExpression.findIndex(char => char === operator);
             }
         }
 
-        maxNum = Math.max(maxNum, Math.abs(allSplit[0]));
-
-        return;
+        answer = Math.max(answer, Math.abs(splitExpression[0]));
     }
 
-    for (const operator of basicOperators) {
-        if (operators.includes(operator)) {
-            continue;
-        }
-        
-        DFS(expression, [...operators, operator]);
-    }
-}
-
-function solution(expression) {
-    DFS(expression, []);
-
-    return maxNum;
+    return answer;
 }
 
 console.log(60420,solution("100-200*300-500+20"));
